c9fb20cb4359c2cabc70bbd0fbb65457ab265833
Wayne Lee <waynel1996weiyang@gmail.com>
2021-07-22 11:02:54 -0700

    Pr fixes

--- client/src/components/ActiveChat/Header.js
+++ client/src/components/ActiveChat/Header.js
@@ -9,7 +9,7 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     justifyContent: "space-between",
     height: "10%",
-    marginBottom: "5%",
+    marginBottom: theme.spacing(),
     boxShadow: "0 2px 20px 0 rgba(88,133,196,0.10)"
   },
   content: {
--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -33,7 +33,7 @@ const Messages = (props) => {
           <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
         );
       })}
-      <div ref={messagesEndRef}/>
+      <Box ref={messagesEndRef}/>
     </Box>
   );
 };
